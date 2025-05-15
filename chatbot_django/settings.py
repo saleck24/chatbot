8528e7decd328af78443e06c7aba5ed600e2cd99
Saleck_Baya <150080341+saleck24@users.noreply.github.com>
2025-05-15 10:18:35 +0000

    Update settings.py

--- chatbot_django/settings.py
+++ chatbot_django/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.csrf',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
